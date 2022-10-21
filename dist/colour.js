87851b418d7588444bd30314cf3092669ce78461
nrtspdn <>
2022-10-21 22:23:30 +0200

    Remove dist folder

--- dist/colour.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY = exports.BLUE = exports.RED = void 0;
-exports.RED = { name: 'red', value: 31 };
-exports.BLUE = { name: 'blue', value: 34 };
-exports.EMPTY = { name: 'empty', value: 10 };
--- dist/common.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.colourText = void 0;
-function colourText(text, colour) {
-    return `\x1b[${colour.value}m${text}\x1b[0m`;
-}
-exports.colourText = colourText;
